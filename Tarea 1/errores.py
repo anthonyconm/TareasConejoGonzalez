480084b65a426c11d940ee6055c267112070fb3a
anthonyconm <anthony266356@gmail.com>
2024-02-16 03:02:51 -0600

    archivo errores.py corregido

--- Tarea 1/errores.py
+++ Tarea 1/errores.py
@@ -1,9 +1,8 @@
-# errores.py
+# errores.py corregidos
 
-# Error E272: multiple spaces before keyword.
+def saludar():
+    print("Hola, mundo!")
 
-def saludar():    print("Hola, mundo!")  # Error E501: line too long (85 > 79 characters).
 
-# Error E302: expected 2 blank lines, found 1
 def despedida():
     print("Adiós!")
