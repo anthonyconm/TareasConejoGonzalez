3a78be907ee40fc561523db9a82c01a3d6ef662e
anthonyconm <anthony266356@gmail.com>
2024-02-16 02:45:35 -0600

    + archivo con 3 errores según flake8

---
+++ Tarea 1/errores.py
@@ -0,0 +1,9 @@
+# errores.py
+
+# Error E272: multiple spaces before keyword.
+
+def saludar():    print("Hola, mundo!")  # Error E501: line too long (85 > 79 characters).
+
+# Error E302: expected 2 blank lines, found 1
+def despedida():
+    print("Adiós!")
